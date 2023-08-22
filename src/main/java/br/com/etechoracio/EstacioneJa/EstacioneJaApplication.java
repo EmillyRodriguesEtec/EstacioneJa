5607399575fcb8144cdb7e6fae96c4e26844865b
EmillyVitoria <EmillyVitoria@example>
2023-08-22 10:39:37 -0300

    Primeiro commit

---
+++ src/main/java/br/com/etechoracio/EstacioneJa/EstacioneJaApplication.java
@@ -0,0 +1,13 @@
+package br.com.etechoracio.EstacioneJa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstacioneJaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstacioneJaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/etechoracio/EstacioneJa/EstacioneJaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.etechoracio.EstacioneJa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstacioneJaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
